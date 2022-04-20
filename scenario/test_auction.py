05092d32496da9af748b2b98c2f2178eec2a9221
Julian Konchunas <conscio@i.ua>
2022-04-20 21:05:02 +0100

    Add auction tests

--- scenario/test_auction.py
+++ scenario/test_auction.py
@@ -18,23 +18,141 @@ class AuctionTest(TestCase):
         text = open("./build/auction.json").read()
         code = json.loads(text)
 
-        cls.dex = ContractInterface.from_micheline(code["michelson"])
+        cls.ct = ContractInterface.from_micheline(code["michelson"])
 
-        storage = cls.dex.storage.dummy()
+        storage = cls.ct.storage.dummy()
         storage["auction_lambdas"] = auction_lambdas
         storage["storage"]["admin"] = admin 
         storage["storage"]["dex_core"] = dex_core
+        storage["storage"]["auction_duration"] = 300
+        storage["storage"]["min_bid"] = 10
+        storage["storage"]["quipu_token"] = {
+            "token" : quipu_token,
+            "id" : 0,
+        }
 
         cls.init_storage = storage
 
     def test_launch_auction(self):
         chain = LocalChain(storage=self.init_storage)
 
-        res = chain.execute(self.dex.receive_fee(token_a_fa2, 10), sender=dex_core)
+        res = chain.execute(self.ct.receive_fee(token_a_fa2, 10), sender=dex_core)
 
-        res = chain.execute(self.dex.launch_auction(token_a_fa2, 10, 100))
+        res = chain.execute(self.ct.launch_auction(token_a_fa2, 10, 100), sender=alice)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 1)
+        self.assertEqual(transfers[0]["amount"], 100)
+        self.assertEqual(transfers[0]["destination"], contract_self_address)
+        self.assertEqual(transfers[0]["source"], alice)
+        self.assertEqual(transfers[0]["token_address"], quipu_token)
+        self.assertEqual(transfers[0]["token_id"], 0)
 
-        pprint(res.storage["storage"])
-        # votes = parse_votes(res)
-        # self.assertEqual(votes[0]["delegate"], carol)
-        # self.assertEqual(votes[0]["amount"], 50 + 0)
+        res = chain.execute(self.ct.place_bid(0, 107), sender=bob)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 2)
+        self.assertEqual(transfers[0]["amount"], 100)
+        self.assertEqual(transfers[0]["destination"], alice)
+        self.assertEqual(transfers[0]["source"], contract_self_address)
+        self.assertEqual(transfers[0]["token_address"], quipu_token)
+        self.assertEqual(transfers[0]["token_id"], 0)
+
+        self.assertEqual(transfers[1]["amount"], 107)
+        self.assertEqual(transfers[1]["destination"], contract_self_address)
+        self.assertEqual(transfers[1]["source"], bob)
+        self.assertEqual(transfers[1]["token_address"], quipu_token)
+        self.assertEqual(transfers[1]["token_id"], 0)
+
+        chain.advance_blocks(10)
+
+        res = chain.execute(self.ct.claim(0))
+        transfers = parse_transfers(res)
+        self.assertEqual(transfers[0]["amount"], 10)
+        self.assertEqual(transfers[0]["destination"], bob)
+        self.assertEqual(transfers[0]["source"], contract_self_address)
+        self.assertEqual(transfers[0]["token_address"], token_a_address)
+        self.assertEqual(transfers[0]["token_id"], 0)
+
+        with self.assertRaises(MichelsonRuntimeError) as error:
+            chain.execute(self.ct.place_bid(0, 150), sender=bob)
+        self.assertEqual(error.exception.args[-1], Errors.AUCTION_FINISHED)
+
+
+    def test_cant_launch(self):
+        chain = LocalChain(storage=self.init_storage)
+
+        with self.assertRaises(MichelsonRuntimeError) as error:
+            chain.execute(self.ct.launch_auction(token_a_fa2, 10, 100))
+        self.assertEqual(error.exception.args[-1], Errors.AUCTION_INSUFFICIENT_BALANCE)
+
+        chain.execute(self.ct.receive_fee(token_a_fa2, 10), sender=dex_core)
+
+        chain.execute(self.ct.set_min_bid(35), sender=admin)
+
+        with self.assertRaises(MichelsonRuntimeError) as error:
+            chain.execute(self.ct.launch_auction(token_a_fa2, 10, 34))
+        self.assertEqual(error.exception.args[-1], Errors.MIN_BID)
+    
+    # TODO
+    def test_bid_fee(self):
+        storage = self.init_storage.copy()
+        storage["storage"]["fees"] = {
+            "bid_fee_f" : int(0.02 * 1e18),
+            "dev_fee_f" : 0
+        }
+        storage["storage"]["min_bid"] = 1
+
+        chain = LocalChain(storage=storage)
+
+        res = chain.execute(self.ct.receive_fee(token_a_fa2, 10), sender=dex_core)
+
+        res = chain.execute(self.ct.launch_auction(token_a_fa2, 10, 1), sender=alice)
+
+        res = chain.execute(self.ct.place_bid(0, 100), sender=bob)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 2)
+        self.assertEqual(transfers[0]["amount"], 1)
+        self.assertEqual(transfers[0]["destination"], alice)
+        self.assertEqual(transfers[0]["source"], contract_self_address)
+        self.assertEqual(transfers[0]["token_address"], quipu_token)
+        self.assertEqual(transfers[0]["token_id"], 0)
+
+        self.assertEqual(transfers[1]["amount"], 100)
+        self.assertEqual(transfers[1]["destination"], contract_self_address)
+        self.assertEqual(transfers[1]["source"], bob)
+        self.assertEqual(transfers[1]["token_address"], quipu_token)
+        self.assertEqual(transfers[1]["token_id"], 0)
+
+        # no fee to burn due to small amounts
+        res = chain.interpret(self.ct.burn_bid_fee(), sender=admin)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 1)
+        self.assertEqual(transfers[0]["amount"], 33)
+
+        res = chain.execute(self.ct.place_bid(0, 1_000), sender=alice)
+
+        res = chain.execute(self.ct.place_bid(0, 10_000), sender=bob)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 2)
+        self.assertEqual(transfers[0]["amount"], 980)
+        self.assertEqual(transfers[0]["destination"], alice)
+        self.assertEqual(transfers[0]["source"], contract_self_address)
+        self.assertEqual(transfers[0]["token_address"], quipu_token)
+        self.assertEqual(transfers[0]["token_id"], 0)
+
+        self.assertEqual(transfers[1]["amount"], 10_000)
+        self.assertEqual(transfers[1]["destination"], contract_self_address)
+        self.assertEqual(transfers[1]["source"], bob)
+        self.assertEqual(transfers[1]["token_address"], quipu_token)
+        self.assertEqual(transfers[1]["token_id"], 0)
+
+        res = chain.execute(self.ct.burn_bid_fee(), sender=admin)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 1)
+        self.assertEqual(transfers[0]["amount"], 33)
+        self.assertEqual(transfers[0]["destination"], burn)
+        self.assertEqual(transfers[0]["source"], contract_self_address)
+
+        # no more feee left to burn
+        res = chain.execute(self.ct.burn_bid_fee(), sender=admin)
+        transfers = parse_transfers(res)
+        self.assertEqual(len(transfers), 0)
