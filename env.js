17b81714fc34fc7c92e8604f915dc0fa502dec91
KStasi <nastasiakondaurova@gmail.com>
2022-05-19 08:14:06 +0300

    Update env for fast tests

--- env.js
+++ env.js
@@ -2,22 +2,21 @@ import accounts from "./scripts/sandbox/accounts";
 
 export default {
   confirmationPollingTimeoutSecond: 500000,
-  syncInterval: 5000, // 0 for tests, 5000 for deploying
-  confirmTimeout: 180000, // 90000 for tests, 180000 for deploying
+  syncInterval: 0, // 0 for tests, 5000 for deploying
+  confirmTimeout: 90000, // 90000 for tests, 180000 for deploying
   buildDir: "build",
   migrationsDir: "migrations",
   contractsDir: "contracts/main",
   ligoVersion: "next",
-  network: "ithacanet",
+  network: "jakartanet",
   networks: {
     development: {
       rpc: "http://localhost:8732",
       network_id: "*",
       secretKey: accounts.alice.sk,
     },
-    ithacanet: {
+    jakartanet: {
       rpc: "https://jakartanet.tezos.marigold.dev",
-      // rpc: "https://rpc.ithacanet.teztnets.xyz",
       port: 443,
       network_id: "*",
       secretKey: accounts.dev.sk,
