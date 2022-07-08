757422fef5f2567cee8840abe9256048965ba147
KStasi <nastasiakondaurova@gmail.com>
2022-07-08 13:51:51 +0300

    support ghostnet

--- env.js
+++ env.js
@@ -2,18 +2,24 @@ import accounts from "./scripts/sandbox/accounts";
 
 export default {
   confirmationPollingTimeoutSecond: 500000,
-  syncInterval: 0, // 0 for tests, 5000 for deploying
-  confirmTimeout: 90000, // 90000 for tests, 180000 for deploying
+  syncInterval: 5000, // 0 for tests, 5000 for deploying
+  confirmTimeout: 180000, // 90000 for tests, 180000 for deploying
   buildDir: "build",
   migrationsDir: "migrations",
   contractsDir: "contracts/main",
   ligoVersion: "0.45.0",
-  network: "development",
+  network: "ghostnet",
   networks: {
     development: {
       rpc: "http://localhost:8732",
       network_id: "*",
       secretKey: accounts.alice.sk,
+    },
+    ghostnet: {
+      rpc: "https://rpc.ghostnet.teztnets.xyz",
+      port: 443,
+      network_id: "*",
+      secretKey: accounts.dev.sk,
     },
     jakartanet: {
       rpc: "https://jakartanet.tezos.marigold.dev",
